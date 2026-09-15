828a5209fe68e5801720321a3b65a000ea6d3032
Kelsey-Patti <kelseypattison1@gmail.com>
2026-09-15 10:51:24 -0600

    added tests and sources

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -4,17 +4,20 @@
 #include <cassert>
 
 //sources 
-// https://www.geeksforgeeks.org/cpp/templates-cpp/  :  Creating class template
+// https://www.geeksforgeeks.org/cpp/templates-cpp/  :  Creating class template and helped with constructor classes 
 // https://stackoverflow.com/questions/3106110/what-is-move-semantics : information and code examples about move semantics
 // https://en.cppreference.com/cpp/utility/forward : for information about perfect forwarding
+// https://www.geeksforgeeks.org/cpp/const-member-functions-c/ : information about when to make a function const
+// https://en.cppreference.com/cpp/language/friend?utm_source=chatgpt.com : for information about using template <typename U> friend class UniquePtr; in private
+// https://www.geeksforgeeks.org/cpp/use-of-explicit-keyword-in-cpp/?utm_source=chatgpt.com : information about when to use explicit 
 
 // Your implementation here
-template <typename T> 
+template <typename T>  
 class UniquePtr {
 private:
     T* ptr;
-    template <typename U> //
-    friend class UniquePtr; //
+    template <typename U> 
+    friend class UniquePtr; 
 public:
     UniquePtr(): ptr(nullptr) {}
     UniquePtr(T* val): ptr(val) {}
@@ -29,7 +32,7 @@ public:
     }
 
     //converting constructor
-    template <typename U> //
+    template <typename U> 
     UniquePtr(UniquePtr<U>&& temp){
         ptr = temp.ptr;
         temp.ptr = nullptr;
@@ -54,43 +57,51 @@ public:
         delete ptr;
     }
 
+    //dereference operator
     T& operator*()const{
         assert(ptr != nullptr);
         return *ptr;
     }
 
+    //arrow operator
     T* operator->() const{
         assert(ptr != nullptr);
         return ptr;
     }
 
+    // get the raw pointer
     T* get() const{
         return ptr;
     }
 
+    // check if two UniquePtr have the same raw pointer
     bool operator==(const UniquePtr& secondPtr) const {
         return ptr == secondPtr.ptr;
     }
 
+    //releases ownership of raw pointer
     T* release(){
         T* tempPtr = ptr;
         ptr = nullptr;
         return tempPtr;
     }
 
+    //reset the raw pointer to a new value
     void reset(T* newPtr = nullptr){
-        assert(newPtr != ptr || newPtr == nullptr); //
+        assert(newPtr != ptr || newPtr == nullptr); 
         delete ptr;
         ptr = newPtr;
     }
     
+    //swaps the raw pointers of two UniquePtrs
     void swap(UniquePtr<T>& other){
         T* temp = other.ptr;
         other.ptr = ptr;
         ptr = temp;
     }
 
-    explicit operator bool() const{
+    // checks if the raw pointer is non-empty
+    explicit operator bool() const{ //used chatGPT to find the keyword explicit then used https://www.geeksforgeeks.org/cpp/use-of-explicit-keyword-in-cpp/?utm_source=chatgpt.com to find how and why to use it
         return ptr!=nullptr;
     }
 };
@@ -98,7 +109,7 @@ public:
 template <typename T, typename... Args>
 UniquePtr<T> makeUnique(Args&&... args)
 {
-    return UniquePtr<T>(new T(std::forward<Args>(args)...)); //
+    return UniquePtr<T>(new T(std::forward<Args>(args)...)); // used ChatGPT to find syntax for forwarding a variable number of arguments
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include "UniquePtr.h"
+#include <iostream>
+#include <cassert>
+#include <utility>
 
 int main() {
-    // UniquePtr<int> ptr(new int(5));
-    // they test all methods here...
+    
+    UniquePtr<int> ptr;
+    assert(!ptr);
+    assert(ptr.get() == nullptr);
+
+    UniquePtr<int> ptr2(new int(7));
+    assert(ptr2.get() != nullptr);
+    assert(*ptr2 ==7);
+
+    *ptr2 = 10;
+    assert(*ptr2.get() == 10);
+
+    UniquePtr<int> ptr3(new int(10));
+    UniquePtr<int> ptr4(new int(20));
+
+    ptr3.swap(ptr4);
+
+    assert(*ptr3 == 20);
+    assert(*ptr4 == 10);
+
+
     return 0;
 }
